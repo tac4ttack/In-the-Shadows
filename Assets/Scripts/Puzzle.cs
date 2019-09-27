72551728b0ddf5e4d446fe03479e3ea52cc0a2b9
Florian MESSINA <fmessina@e1r8p21.42.fr>
2019-09-27 12:50:37 +0200

    wip: Base puzzle system in progress, orientation check ok

---
+++ Assets/Scripts/Puzzle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    public Light            debugSpot;
+    public PuzzlePiece[]    pieces;
+
+    private bool            validated;
+
+    void Start()
+    {
+        validated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPieces();
+        if (validated)
+            debugSpot.color = Color.green;
+        else
+            debugSpot.color = Color.white;
+    }
+
+    void CheckPieces()
+    {
+        bool   tmp = false;
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            PuzzlePiece p = (PuzzlePiece)pieces[i];
+            tmp = p.validated;
+        }
+        validated = tmp;
+    }
+}
---
+++ Assets/Scripts/PuzzlePiece.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    [HideInInspector] public      bool            validated;
+    public      Quaternion[]    solQua;
+    public      Vector3[]       solPos;
+
+    private     float           speed = 21f;
+    private     float           quaBias = 0.035f;
+    private     float           posBias = 0.035f;
+    private     bool            quaOK;
+    private     bool            posOK;
+    private     Quaternion      currentQua;
+    private     Vector3         currentPos;
+
+    void Start()
+    {
+        validated = false;
+        quaOK = false;
+        posOK = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(this.gameObject.transform.localRotation);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+            this.gameObject.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed);
+        
+        if (Input.GetKey(KeyCode.I))
+            this.gameObject.transform.Rotate(new Vector3(1, 0, 0), 0.5f);
+
+        if (Input.GetKey(KeyCode.K))
+            this.gameObject.transform.Rotate(new Vector3(1, 0, 0), -0.5f);
+
+        if (Input.GetKey(KeyCode.J))
+            this.gameObject.transform.Rotate(new Vector3(0, 1, 0), 0.5f);
+
+        if (Input.GetKey(KeyCode.L))
+            this.gameObject.transform.Rotate(new Vector3(0, 1, 0), -0.5f);
+
+        if (Input.GetKey(KeyCode.U))
+            this.gameObject.transform.Rotate(new Vector3(0, 0, 1), 0.5f);
+
+        if (Input.GetKey(KeyCode.O))
+            this.gameObject.transform.Rotate(new Vector3(0, 0, 1), -0.5f);
+
+
+        currentQua = this.gameObject.transform.localRotation;
+        currentPos = this.gameObject.transform.localPosition;
+
+        CheckSolutions();
+    }
+
+    /*
+        Will check if the puzzle piece is correctly rotated and translated.
+        Quaternions are logically equal when:
+            Q1 == Q2
+            or
+            Q1 == -Q2
+     */
+    private void    CheckSolutions()
+    {
+        for (int i = 0; i < solQua.Length; i++)
+        {
+            Quaternion q = (Quaternion)solQua[i];
+            if (    ((q.x >=  currentQua.x - quaBias && q.x <=  currentQua.x + quaBias)
+                &&   (q.y >=  currentQua.y - quaBias && q.y <=  currentQua.y + quaBias)
+                &&   (q.z >=  currentQua.z - quaBias && q.z <=  currentQua.z + quaBias)
+                &&   (q.w >=  currentQua.w - quaBias && q.w <=  currentQua.w + quaBias))
+                ||
+                    ((q.x >= -currentQua.x - quaBias && q.x <= -currentQua.x + quaBias)
+                &&   (q.y >= -currentQua.y - quaBias && q.y <= -currentQua.y + quaBias)
+                &&   (q.z >= -currentQua.z - quaBias && q.z <= -currentQua.z + quaBias)
+                &&   (q.w >= -currentQua.w - quaBias && q.w <= -currentQua.w + quaBias)))
+            {
+                quaOK = true;
+                break;
+            }
+            else
+            {
+                quaOK = false;
+            }
+        }
+
+        for (int i = 0; i < solPos.Length; i++)
+        {
+            Vector3 p = (Vector3)solPos[i];
+            if (    (p.x >=  currentPos.x - posBias && p.x <=  currentPos.x + posBias)
+                &&  (p.y >=  currentPos.y - posBias && p.y <=  currentPos.y + posBias)
+                &&  (p.z >=  currentPos.z - posBias && p.z <=  currentPos.z + posBias))
+            {
+                posOK = true;
+                break;
+            }
+            else
+            {
+                posOK = false;
+            }
+        }
+
+        if (quaOK && posOK)
+            validated = true;
+        else
+            validated = false;
+    }
+}
