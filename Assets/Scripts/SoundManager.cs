09ef5f483f56efb5d24614362772d728770d5f59
Florian MESSINA <fmessina@e1r5p22.42.fr>
2019-10-02 16:51:36 +0200

    dev: Began scripting the sound manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager sm { get; private set; }
+
+    public List<AudioClip> Musics = new List<AudioClip>();
+    public List<AudioClip> Sounds = new List<AudioClip>();
+    public AudioSource CommonSrc;
+
+    private void Awake()
+    {
+        if (!CommonSrc)
+            CommonSrc = GetComponent<AudioSource>();
+        if (!sm)
+            sm = this;
+    }
+}
