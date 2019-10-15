b0013caa27820a1955752696a7cf109c70a086ba
Florian MESSINA <fmessina@e3r2p18.42.fr>
2019-10-15 18:23:23 +0200

    wip: Main menu in progress, credits poc ok, settings layout ok

---
+++ Assets/Scripts/CreditsAutoscroll.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CreditsAutoscroll : MonoBehaviour
+{
+    public float Speed = 0.01f;
+    
+    private UnityEngine.UI.Scrollbar _bar;
+    private bool _paused = false;
+    private float _delay = 0f;
+    
+    void Awake()
+    {
+        _bar = GetComponent<UnityEngine.UI.Scrollbar>();
+        Assert.IsNotNull(_bar, "Scroll bar component not found!");
+    }
+    void OnEnable()
+    {
+        _bar.value = 1f;
+        _delay = 0f;
+    }
+
+    void Update()
+    {
+        _delay += 0.01f;
+
+        if (!_paused && (_delay > 1f))
+            _bar.value = _bar.value - Time.deltaTime * Speed;
+    }
+
+    
+    public void ScrollPause()
+    {
+        _paused = true;
+    }
+
+    public void ScrollResume()
+    {
+        _paused = false;
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,7 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject MainPanel;
+    public GameObject SettingsPanel;
+    public GameObject CreditsPanel;
+
+    private GameObject _activePanel;
+
+    void Start()
+    {
+        Assert.IsNotNull(MainPanel, "Main panel GameObject not set!");
+        Assert.IsNotNull(SettingsPanel, "Settings panel GameObject not set!");
+        Assert.IsNotNull(CreditsPanel, "Settings panel GameObject not set!");
+
+        MainPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        _activePanel = MainPanel;
+    }
+
     public void ExitButton()
     {
         Debug.Log("Exiting...");
@@ -10,6 +29,22 @@ public class MainMenu : MonoBehaviour
 
     public void BackButton()
     {
-        Debug.Log("Supposed to go back...");
+        _activePanel.SetActive(false);
+        MainPanel.SetActive(true);
+        _activePanel = MainPanel;
+    }
+
+    public void SettingsButton()
+    {
+        MainPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+        _activePanel = SettingsPanel;
+    }
+
+    public void CreditsButton()
+    {
+        MainPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+        _activePanel = CreditsPanel;
     }
 }
