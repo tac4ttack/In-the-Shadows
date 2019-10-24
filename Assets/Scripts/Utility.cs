d0d93ba2a9b2d0c38ba50ac2ee8f7d851f206808
Florian MESSINA <fmessina@e3r2p18.42.fr>
2019-10-24 11:55:27 +0200

    feat: Created an utility class with generic functions

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public static class Utility
+{
+    public static bool IsPointerOverUIObject()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+
+        eventData.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        EventSystem.current.RaycastAll(eventData, raycastResults);
+        
+        return (raycastResults.Count > 0);
+    }
+}
