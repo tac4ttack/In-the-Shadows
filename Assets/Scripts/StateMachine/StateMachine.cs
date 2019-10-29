8d9118d17d8a9dbcf038b328a61c4bed7037bad8
Florian MESSINA <fmessina@e3r2p18.42.fr>
2019-10-29 14:54:59 +0100

    feat: Added a StateMachine and IState classes

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,39 @@
+﻿
+// using UnityEngine;
+
+// public class StateMachine : MonoBehaviour
+public class StateMachine
+{
+    private IState _CurrentState;
+    private IState _PreviousState;
+
+    public void ChangeState(IState newState)
+    {
+        if (_CurrentState != null)
+            _CurrentState.Exit();
+        _PreviousState = _CurrentState;
+        _CurrentState = newState;
+        _CurrentState.Enter();
+    }
+
+    public void ExecuteState()
+    {
+        if (_CurrentState != null)
+            _CurrentState.Execute();
+    }
+
+    public void GoBackToPreviousState()
+    {
+        if (_PreviousState != null)
+            ChangeState(_PreviousState);
+    }
+
+    /*
+    public void SwitchToPreviousState()
+    {
+        _CurrentState.Exit();
+        _CurrentState = _PreviousState;
+        _CurrentState.Enter();
+    }
+    */
+}
