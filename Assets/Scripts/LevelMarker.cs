9bdf9f71442e6442701d8981aaf98776ea7fb8a9
Florian MESSINA <fmessina@e3r2p18.42.fr>
2019-11-01 16:03:47 +0100

    wip: Level Selection scene in progress!

---
+++ Assets/Scripts/LevelMarker.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class LevelMarker : MonoBehaviour
+{
+    public enum LevelStatus {Locked = 0, Unlocked, Completed};
+
+    [SerializeField] private int _Id = -1;
+    [SerializeField] private string _Title = "unset!";
+    [SerializeField] private Vector3 _Position = new Vector3(0f, 0f, 0f);
+    [SerializeField] private LevelStatus  _Status = LevelStatus.Locked;
+    [SerializeField][TextArea(3,10)] private string _Description = "empty!";
+    [SerializeField] private string _BestTime = "00:00:00"; // -> find a better type for this?
+    
+    public int Id { get => _Id; }
+    public string Title { get => _Title; }
+    public Vector3 Position { get => _Position; }
+    public LevelStatus  Status { get => _Status; }
+    public string Description { get => _Description; }
+    public string BestTime { get => _BestTime; }
+
+    void Awake()
+    {
+        if (Id == -1)
+            Debug.LogError("ERROR in [" + this.gameObject.name + "]: LevelMarker ID is invalid!");
+        if (Title == "unset!")
+            Debug.LogError("ERROR in [" + this.gameObject.name + "]: LevelMarker TITLE seems unset!");
+        if (Description == "empty!")
+            Debug.LogError("ERROR in [" + this.gameObject.name + "]: LevelMarker DESCRIPTION seems empty!");
+    }
+
+    void Start()
+    {
+        if (Id >= 0)
+        {
+            _Position = new Vector3(Random.Range(0f, 2f), Random.Range(0f, 2f), Random.Range(0f, 2f));
+            
+            // Fetch the level status
+            // _Status = LevelStatus[GameManager.GM.Players[CurrentPlayer].Levels[LevelId]];
+
+            // Best time fetch from player data!
+            // _BestTime = GameManager.GM.Players[CurrentPlayer].BestTimes[LevelId];
+            _BestTime = (Random.Range(0, 24)).ToString("D2")
+                + ":" + (Random.Range(0, 60)).ToString("D2")
+                + ":" + (Random.Range(0, 60)).ToString("D2");
+
+        }
+    }
+}
---
+++ Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using TMPro;
+
+public class LevelSelection : MonoBehaviour
+{
+    [Header("Elements of Level Selection UI")]
+    public Button NavLeft_BTN;
+    public Button NavRight_BTN;
+    public TextMeshProUGUI LevelTitle_TXT;
+    public TextMeshProUGUI LevelCount_TXT;
+
+    public TextMeshProUGUI LevelDescriptionTitle_TXT;
+    public TextMeshProUGUI LevelDescriptionContent_TXT;
+    public TextMeshProUGUI LevelDescriptionBestTime_TXT;
+    public Button LevelDescriptionPlay_BTN;
+
+    public LevelMarker[] Levels;
+
+    void Awake()
+    {
+
+        if (NavLeft_BTN == null)
+            NavLeft_BTN = GameObject.FindGameObjectWithTag("LevelSelection_NavLeftButton").GetComponent<Button>();
+        Assert.IsNotNull(NavLeft_BTN, "Left Navigation button not found!");
+
+        if (NavRight_BTN == null)
+            NavRight_BTN = GameObject.FindGameObjectWithTag("LevelSelection_NavRightButton").GetComponent<Button>();
+        Assert.IsNotNull(NavRight_BTN, "Right Navigation button not found!");
+
+        if (LevelTitle_TXT == null)
+            LevelTitle_TXT = GameObject.FindGameObjectWithTag("LevelSelection_LevelTitleText").GetComponent<TextMeshProUGUI>();
+        Assert.IsNotNull(LevelTitle_TXT, "Level Title text placeholder not found!");
+
+        if (LevelCount_TXT == null)
+            LevelCount_TXT = GameObject.FindGameObjectWithTag("LevelSelection_LevelCountText").GetComponent<TextMeshProUGUI>();
+        Assert.IsNotNull(LevelCount_TXT, "Level Count text placeholder not found!");
+
+        Levels = GameObject.FindGameObjectWithTag("LevelSelection_Map").GetComponentsInChildren<LevelMarker>();
+        if (Levels.Length <= 0)
+            Debug.LogError("Level list can't be empty!");
+    }
+
+    void Start()
+    {
+        // DEBUG
+        // Load here the information to fill the texts!
+        LevelTitle_TXT.text = "Foobar!";
+        LevelCount_TXT.text = "66/66";
+    }
+}
