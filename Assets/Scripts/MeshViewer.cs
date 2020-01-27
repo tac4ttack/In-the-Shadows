a48cc0ed1047237aa77c10fc0497df036088294d
Florian MESSINA <fmessina@student.42.fr>
2020-01-27 16:45:08 +0100

    assets: Updated the globes meshes and assets

--- Assets/Scripts/MeshViewer.cs
+++ Assets/Scripts/MeshViewer.cs
@@ -7,7 +7,7 @@ public class MeshViewer : MonoBehaviour
     [SerializeField] private Vector3 _RotationAxis = new Vector3(0, 1, 0);
     [SerializeField] private Vector3 _ScaleFactor = new Vector3(1f, 1f, 1f);
     [SerializeField] private Mesh _Mesh = null;
-    [SerializeField] private Material _Material = null;
+    [SerializeField] private Material[] _Materials = new Material[1];
 
     private MeshFilter _MeshFilter;
     private MeshRenderer _MeshRenderer;
@@ -23,11 +23,16 @@ public class MeshViewer : MonoBehaviour
         Assert.IsNotNull(_MeshRenderer, "No mesh renderer component found on mesh viewer!");
 
         Assert.IsNotNull(_Mesh, "No mesh set for win screen!");
-        Assert.IsNotNull(_Material, "No material set for win screen!");
+        Assert.IsTrue(_Materials.Length > 0, "Materials list is empty!");
 
-        _MeshFilter.mesh = _Mesh;
-        _MeshRenderer.material = _Material;
+        _MeshFilter.mesh = _Mesh;   
         this.transform.localScale = _ScaleFactor;
+        _MeshRenderer.materials = new Material[_Materials.Length];
+        Material[] tmp = _MeshRenderer.materials;
+        for (int i = 0; i < _Materials.Length; i++)
+            tmp[i] = _Materials[i];
+        _MeshRenderer.materials = tmp;
+
     }
 
     void Update()
