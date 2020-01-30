b8bc4d3784e53e9b1baeade1b2d6ddfbf2c61b9d
F.Messina - Tac <fmessina@student.42.fr>
2020-01-30 12:09:28 +0100

    feat: Added a random start rotation to the earth

--- Assets/Scripts/EarthRotate.cs
+++ Assets/Scripts/EarthRotate.cs
@@ -5,6 +5,11 @@ public class EarthRotate : MonoBehaviour
     [SerializeField] private float _RotationSpeed = 1.0f;
     [SerializeField] private Vector3 _RotationAxis = new Vector3(0, 1, 0);
 
+    void Start()
+    {
+        this.transform.localRotation = Quaternion.Euler(0f, Random.Range(0f, 359f), 0f);
+    }
+    
     void Update()
     {
         this.transform.Rotate(_RotationAxis, -1 * _RotationSpeed * Time.deltaTime);
