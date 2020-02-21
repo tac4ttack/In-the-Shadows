1509807f30dab5a366a8fac2cdc3c6b71f11fb29
Florian MESSINA <fmessina@student.42.fr>
2020-02-21 15:25:24 +0100

    feat: Credits seems ok

--- Assets/Scripts/CreditsAutoscroll.cs
+++ Assets/Scripts/CreditsAutoscroll.cs
@@ -1,31 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Assertions;
+using TMPro;
 
 public class CreditsAutoscroll : MonoBehaviour
 {
-    public float Speed = 0.01f;
-
-    private UnityEngine.UI.Scrollbar _Bar;
+    private Scrollbar _Bar;
+    [SerializeField] private float _Speed = 0.006f;
     private bool _Paused = false;
     private float _Delay = 0f;
+    private string _Credits_Text;
+    private Button _Credits_BTN;
+    private TextMeshProUGUI _Credits_TextContainer;
 
     void Awake()
     {
-        _Bar = GetComponent<UnityEngine.UI.Scrollbar>();
+        if (_Bar == null)
+            _Bar = GetComponent<UnityEngine.UI.Scrollbar>();
         Assert.IsNotNull(_Bar, "Scroll bar component not found!");
-    }
-    void OnEnable()
-    {
+        _Bar.value = 1f;
+
+        _Credits_Text = Resources.Load("Credits").ToString();
+        if (_Credits_Text.Length == 0)
+            _Credits_Text = "Credits text is missing!";
+
+        if (_Credits_BTN == null)
+            _Credits_BTN = GameObject.FindGameObjectWithTag("MainMenu_CreditsButton").GetComponent<Button>();
+        Assert.IsNotNull(_Credits_BTN, "Credits button not found!");
+        _Credits_BTN.onClick.AddListener(delegate { ResetScroll(); });
+    
+        if (_Credits_TextContainer == null)
+            _Credits_TextContainer = GameObject.FindGameObjectWithTag("MainMenu_CreditsContainer").GetComponent<TextMeshProUGUI>();
+        Assert.IsNotNull(_Credits_TextContainer, "Credits text container not found!");
+        
+        _Credits_TextContainer.text = _Credits_Text;
         _Bar.value = 1f;
         _Delay = 0f;
     }
 
     void Update()
     {
-        _Delay += 0.01f;
+        _Delay += Time.deltaTime;
 
         if (!_Paused && (_Delay > 1f))
-            _Bar.value = _Bar.value - Time.deltaTime * Speed;
+            _Bar.value = _Bar.value - Time.deltaTime * _Speed;
     }
 
     public void ScrollPause()
@@ -36,5 +54,11 @@ public class CreditsAutoscroll : MonoBehaviour
     public void ScrollResume()
     {
         _Paused = false;
+    }
+
+    private void ResetScroll()
+    {
+        _Bar.value = 1f;
+        _Delay = 0f;
     }
 }
